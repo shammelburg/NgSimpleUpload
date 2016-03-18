dcdc62a97c16d53d4142f9a87dcbe9712b738379
Sander Hammelburg <burg_san@live.com>
2016-03-18 08:16:15 +0000

    Update ng-simple-upload.js
    Added manual upload by button functionality.

--- scripts/ng-simple-upload.js
+++ scripts/ng-simple-upload.js
@@ -2,12 +2,34 @@ angular.module('app').directive('ngSimpleUpload', [function () {
     return {
         scope: {
             webApiUrl: '@',
-            callbackFn: '='
+            callbackFn: '=',
+            buttonId: '@'
         },
         link: function (scope, element, attrs) {
-            element.on('change', function (evt) {
-                var files = evt.__files_ || (evt.target && evt.target.files);
+            // if button id value exists
+            if (scope.buttonId) {
+                $('#' + scope.buttonId).on('click', function () {
+                    // retrieves files from file input
+                    var files = element[0].files;
+                    // will not fire until file(s) are selected
+                    if (files.length == 0) {
+                        console.log('No files detected.');
+                        return false;
+                    }
 
+                    Upload(files);
+                });
+            }
+            else {
+                // original code, trigger upload on change
+                element.on('change', function (evt) {
+                    var files = evt.__files_ || (evt.target && evt.target.files);
+
+                    Upload(files);
+                });
+            }
+
+            function Upload(files) {
                 var fd = new FormData();
                 angular.forEach(files, function (v, k) {
                     fd.append('file', files[k]);
@@ -22,11 +44,12 @@ angular.module('app').directive('ngSimpleUpload', [function () {
                     contentType: false,
                     processData: false
                 }).done(function (d) {
+                    // callback function in the controller
                     scope.callbackFn(d);
                 }).fail(function (x) {
                     console.log(x);
                 });
-            });
+            }
         }
     }
 }]);
